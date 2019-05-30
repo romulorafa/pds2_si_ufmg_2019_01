561fa73aaf7ef035a7c2b1dbae2a9d955d5b2007
Rômulo Silva <romulo.rds89@gmail.com>
2019-05-30 08:48:16 -0300

    commit lab 06.00

---
+++ Lab 06.00 - Warmup/include/triangulo.h
@@ -0,0 +1,26 @@
+#ifndef PDS2_TRIANGULO_H
+#define PDS2_TRIANGULO_H
+
+#include <cmath>
+#include <iostream>
+#include "excecoes.h"
+
+using namespace std;
+
+enum Tipo {EQUILATERO, ISOSCELES, ESCALENO};
+
+class Triangulo {
+private:
+    double x, y, z;
+
+public:
+    Triangulo();
+    ~Triangulo();
+    Triangulo(double x, double y, double z);
+
+    int verifica_condicoes(double x, double y, double z);
+    Tipo determinar_tipo();
+
+};
+
+#endif /*   PDS2_TRIANGULO_H    */
---
+++ Lab 06.00 - Warmup/src/triangulo.cpp
@@ -0,0 +1,62 @@
+#include "triangulo.h"
+
+// *CONSTRUTOR
+// **Triangulo - construtor padrão
+Triangulo::Triangulo(){
+
+}
+
+// **Triangulo - construtor com parâmetros
+Triangulo::Triangulo(double x, double y, double z){
+
+    try
+    {
+        if(verifica_condicoes(x, y, z)){
+            this->x = x;
+            this->y = y;
+            this->z = z;
+        }
+    }
+    catch(TrianguloInvalidoException::exception& e)
+    {
+        cerr << e.what() << '\n';
+    }
+}
+
+// *DESTRUTOR
+// **Triangulo - destrutor padrão
+Triangulo::~Triangulo(){
+
+}
+
+// *MÉTODOS
+// **verifica_condições: dados os valores dos lados, verifica se formam um triangulo válido
+int Triangulo::verifica_condicoes(double x, double y, double z){
+
+    if ((this->x <= 0) || (this->y <= 0) || (this->z <= 0)){
+        throw TrianguloInvalidoException();
+    }
+    else if ((abs(y - z) < x) && (x < abs(y + z))){
+        throw TrianguloInvalidoException();
+    }
+
+    return 1;
+}
+// **determinar_tipo: retorna um dos três tipos do triangulo
+Tipo Triangulo::determinar_tipo(){
+
+    if ((this->x == this->y) && (this->y == this->z)){
+        return (EQUILATERO);
+    }
+    else{
+        if ((this->x != y) && (this->y != this->z) &&(this->x != this->z)){
+            return ESCALENO;
+        }
+        else{
+            return ISOSCELES;
+        }
+    }
+}
+
+
+
